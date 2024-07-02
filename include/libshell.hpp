d673223ef3b7c7fafec5aa86fe60c7f7e851cbe1
Jasper1467 <vectorcall@macesunrise.maskmy.id>
2024-07-02 08:03:09 +0200

    modified:   include/libshell.hpp

--- include/libshell.hpp
+++ include/libshell.hpp
@@ -6,17 +6,20 @@
 #include <map>
 #include <sstream>
 #include <string>
+#include <string_view>
 #include <vector>
 
 class CShell
 {
-private:
+public:
     using CommandFn = std::function<void(const std::vector<std::string>)>;
+
+private:
     std::map<std::string, CommandFn> m_mapCommands;
-    std::string m_szPrefix;
+    std::string_view m_szPrefix;
 
 public:
-    explicit CShell(const std::string& szPrefix) : m_szPrefix(szPrefix)
+    explicit CShell(const std::string_view& szPrefix) : m_szPrefix(szPrefix)
     {
     }
 
@@ -30,6 +33,11 @@ public:
         m_mapCommands[szCommand] = fnFunction;
     }
 
+    void UnregisterCommand(std::string szCommand)
+    {
+        m_mapCommands.erase(m_mapCommands.find(szCommand));
+    }
+
     enum Status_e
     {
         STATUS_SUCCESS = 0,
@@ -37,16 +45,33 @@ public:
         STATUS_UNKNOWN_COMMAND,
     };
 
+    const std::map<std::string, CommandFn> GetCommands() const
+    {
+        return m_mapCommands;
+    }
+
+    void ClearCommands()
+    {
+        m_mapCommands.clear();
+    }
+
     void SetPrefix(std::string szPrefix)
     {
         m_szPrefix = szPrefix;
+    }
+
+    void ExecuteCommand(std::string szCommand, std::vector<std::string> vecArgs)
+    {
+        auto it = m_mapCommands.find(szCommand);
+        if (it != m_mapCommands.end())
+            it->second(vecArgs);
     }
 
     Status_e Run()
     {
         std::string szInput;
 
-        printf(m_szPrefix.c_str());
+        std::cout << m_szPrefix;
         std::getline(std::cin, szInput);
 
         if (szInput.empty())
@@ -72,7 +97,7 @@ public:
     }
 
 private:
-    std::vector<std::string> ParseInput(const std::string& szInput)
+    std::vector<std::string> ParseInput(const std::string szInput)
     {
         std::istringstream iss(szInput);
         std::vector<std::string> vecTokens;
